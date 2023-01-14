c75ee41743c99e00c4216efc45d98a0738f060fb
giolongo <giovanniemanuelelongo@gmail.com>
2023-01-14 13:51:44 +0100

    feat: homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { StudyComponent } from './components/study/study.component';
 import {HttpClientModule} from "@angular/common/http";
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    StudyComponent
+    StudyComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
