bdb963b5ad29e443400eb2b7f2fe095d96cef29b
giolongo <giovanniemanuelelongo@gmail.com>
2023-01-15 11:23:09 +0100

    feat: home message typed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StudyComponent } from './components/study/study.component';
 import {HttpClientModule} from "@angular/common/http";
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {NgxTypedJsModule} from "ngx-typed-js";
 
 @NgModule({
   declarations: [
@@ -20,7 +21,9 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgbModule
+    NgxTypedJsModule,
+    NgbModule,
+    NgxTypedJsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
